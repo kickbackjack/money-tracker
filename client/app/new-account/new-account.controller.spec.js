286e94f2e6c509a34f2b757c9ebc1163d773f31d
Jack Coombes <jackcoombes333@gmail.com>
2016-03-16 04:09:06 -0700

    Added 'New Account Modal/Dialog'

---
+++ client/app/new-account/new-account.controller.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Controller: NewAccountCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('ynabExtensionApp'));
+
+  var NewAccountCtrl, scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    NewAccountCtrl = $controller('NewAccountCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
+});

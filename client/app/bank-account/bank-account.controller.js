263bc1b55c10d3d99c230a687905e85848ec6d70
Jack Coombes <jackcoombes333@gmail.com>
2016-03-17 08:12:02 -0700

    Added transaction directive Added dummy data for transactions

--- client/app/bank-account/bank-account.controller.js
+++ client/app/bank-account/bank-account.controller.js
@@ -2,5 +2,28 @@
 
 angular.module('ynabExtensionApp')
   .controller('BankAccountCtrl', function ($scope) {
-    $scope.message = 'Hello';
+
+    // TODO: Replace with data received from server
+    $scope.transactions = [
+      {
+        datecreated: new Date(),
+        cleared: false,
+        income: 1900.00,
+        outcome: 0,
+        description: 'Salary',
+        category: 'Income',
+        payee: 'Work'
+      },
+      {
+        datecreated: new Date(),
+        cleared: false,
+        income: 0,
+        outcome: 1140.00,
+        description: 'Big Payment',
+        category: 'Mortgage',
+        payee: 'Mortgage Company'
+      }
+    ];
+
+    
   });

c45266c21f78a8bbfa5c9513782d96f3c9f1cc83
Jack Coombes <jackcoombes333@gmail.com>
2016-03-29 23:18:01 +0100

    Minor styling changes Added image logo

--- client/app/directives/transaction-grid/transaction-grid.directive.js
+++ client/app/directives/transaction-grid/transaction-grid.directive.js
@@ -17,6 +17,26 @@ angular.module('moneyBagsApp')
           page: 1
         };
 
+        scope.transactions = [
+          {
+            datecreated: new Date(),
+            cleared: false,
+            income: 1900.00,
+            outcome: 0,
+            description: 'Salary',
+            category: 'Income',
+            payee: 'Work'
+          },
+          {
+            datecreated: new Date(),
+            cleared: false,
+            income: 0,
+            outcome: 1140.00,
+            description: 'Big Payment',
+            category: 'Mortgage',
+            payee: 'Mortgage Company'
+          }];
+
         function getTransactions(query) {
           scope.promise = scope.transactions.get(query, success).$promise;
         }

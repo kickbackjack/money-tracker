020831bccd85b34626cb8eec0ad4895cdd2fac06
Jack Coombes <jackcoombes333@gmail.com>
2016-03-29 18:19:45 +0100

    Updated index.html base layout structure

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('moneyBagsApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'navbar'
+  }));
